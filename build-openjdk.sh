7e2430f0727e4bd424b09e8cbd853103401daca7
ximenpo <ximenpo@jiandan.ren>
2016-07-12 13:33:34 +0800

    add build-openjdk.sh build all scripts.

--- build-openjdk.sh
+++ build-openjdk.sh
@@ -2,11 +2,13 @@
 
 yum install -y  zip unzip
 yum install -y  gcc gcc-c++ libstdc++-static
+yum install -y  glibc-devel.i686    libstdc++.i686  libstdc++-static.i686
 
 X_ROOT_DIR=~/openjdk
 X_ORACLE_JDK_DIR=~/jdk1.8.0_91
 X_APACHE_ANT_DIR=~/apache-ant-1.9.7
 
+############# O1: build hotspot ##############
 unset   JAVA_HOME
 export  LANG=C
 export  ALT_BOOTDIR=$X_ORACLE_JDK_DIR
@@ -19,5 +21,18 @@ cd  $X_ROOT_DIR/hotspot/make
 make    DEBUG_BINARIES=true ARCH_DATA_MODEL=64 jvmg
 
 #   32bit client
-yum install -y  glibc-devel.i686    libstdc++.i686  libstdc++-static.i686
 make    DEBUG_BINARIES=true jvmg1
+
+############# O2: build all ##############
+unset   JAVA_HOME
+export  LANG=C
+export  PATH=$X_ORACLE_JDK_DIR/bin:$PATH
+
+yum install -y  libXtst-devel libXt-devel libXrender-devel
+yum install -y  cups-devel
+yum install -y  freetype-devel
+yum install -y  alsa-lib-devel
+
+cd  $X_ROOT_DIR
+bash    ./configure
+make    all
